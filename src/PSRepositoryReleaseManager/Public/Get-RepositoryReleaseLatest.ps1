8c57cca014ef9da597337c419828a9da65397146
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-09 18:09:12 +0800

    Add module function Get-RepositoryReleaseLatest

---
+++ src/PSRepositoryReleaseManager/Public/Get-RepositoryReleaseLatest.ps1
@@ -0,0 +1,31 @@
+function Get-RepositoryReleaseLatest {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path -Path $_ -PathType Container})]
+        [string]$Path
+    )
+    $_path = Get-Item -Path $PSBoundParameters['Path']
+    Push-Location $_path
+    $ErrorActionPreference = 'Stop'
+
+    try {
+        "Searching for the latest release in the repository '$_path'" | Write-Verbose
+        $releaseTagsInfo = (git --no-pager log --tags --simplify-by-decoration --pretty="format:%H %D") -split "`n" | % {
+            if ($_ -match '\s+tag:\s+(v\d+\.\d+\.\d+)(,|$)') {
+                $_
+            }
+        }
+        if (!$releaseTagsInfo) {
+            throw "No release tags could be found in the repository '$_path'."
+        }
+        "Release tags info:" | Write-Verbose
+        $releaseTagsInfo | Write-Verbose
+        $releaseLatestCommitSHA = ($releaseTagsInfo[0] -split "\s")[0]
+        git tag --points-at $releaseLatestCommitSHA | Sort-Object -Descending
+    }catch {
+        throw
+    }finally {
+        Pop-Location
+    }
+}
