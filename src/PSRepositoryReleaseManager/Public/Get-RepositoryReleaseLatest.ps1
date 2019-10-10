6e6fe92a4978ec6570c160dde920d05237f82496
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-11 05:26:22 +0800

    Remove use of unneeded path variable in Get-RepositoryReleasePrevious and Get-RepositoryReleaseLatest

--- src/PSRepositoryReleaseManager/Public/Get-RepositoryReleaseLatest.ps1
+++ src/PSRepositoryReleaseManager/Public/Get-RepositoryReleaseLatest.ps1
@@ -5,19 +5,18 @@ function Get-RepositoryReleaseLatest {
         [ValidateScript({Test-Path -Path $_ -PathType Container})]
         [string]$Path
     )
-    $_path = Get-Item -Path $PSBoundParameters['Path']
-    Push-Location $_path
+    Push-Location $PSBoundParameters['Path']
     $ErrorActionPreference = 'Stop'
 
     try {
-        "Searching for the latest release in the repository '$_path'" | Write-Verbose
+        "Searching for the latest release in the repository '$($PSBoundParameters['Path'])'" | Write-Verbose
         $releaseTagsInfo = (git --no-pager log --date-order --tags --simplify-by-decoration --pretty="format:%H %D") -split "`n" | % {
             if ($_ -match '\s+tag:\s+(v\d+\.\d+\.\d+)(,|$)') {
                 $_
             }
         }
         if (!$releaseTagsInfo) {
-            throw "No release tags could be found in the repository '$_path'."
+            throw "No release tags could be found in the repository '$($PSBoundParameters['Path'])'."
         }
         "Release tags info:" | Write-Verbose
         $releaseTagsInfo | Write-Verbose
