3a8ca95961e0de0403c0a95ffae817f00c11b91f
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-17 00:48:05 +0800

    Revise error message and improve verbose output for Get-RepositoryReleaseLatest

--- src/PSRepositoryReleaseManager/Public/Get-RepositoryReleaseLatest.ps1
+++ src/PSRepositoryReleaseManager/Public/Get-RepositoryReleaseLatest.ps1
@@ -8,17 +8,18 @@ function Get-RepositoryReleaseLatest {
 
     try {
         Push-Location $PSBoundParameters['Path']
-        "Searching for the latest release in the repository '$($PSBoundParameters['Path'])'" | Write-Verbose
+        "Retrieving info on release tags" | Write-Verbose
         $releaseTagsInfo = (git --no-pager log --date-order --tags --simplify-by-decoration --pretty="format:%H %D") -split "`n" | % {
             if ($_ -match '\s+tag:\s+(v\d+\.\d+\.\d+)(,|$)') {
                 $_
             }
         }
         if (!$releaseTagsInfo) {
-            throw "No release tags could be found in the repository '$($PSBoundParameters['Path'])'."
+            throw "No release tags exist in the repository '$($PSBoundParameters['Path'])'."
         }
         "Release tags info:" | Write-Verbose
         $releaseTagsInfo | Write-Verbose
+        "Retrieving latest release tag(s)" | Write-Verbose
         $releaseLatestCommitSHA = ($releaseTagsInfo[0] -split "\s")[0]
         git tag --points-at $releaseLatestCommitSHA | Sort-Object -Descending       # Returns an array of tags if they point to the same commit
     }catch {
