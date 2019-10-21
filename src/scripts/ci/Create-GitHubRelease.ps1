71215f37857a2b0d31e3c5d21aec48dc1204d5f3
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-22 01:05:49 +0800

    Add entrypoint scripts for CI for abstraction from CI. Revise development scripts bear as much similarity to the CI entrypoint sctipts

---
+++ src/scripts/ci/Create-GitHubRelease.ps1
@@ -0,0 +1,32 @@
+[CmdletBinding()]
+param()
+
+$ErrorActionPreference = 'Stop'
+$VerbosePreference = 'Continue'
+Set-StrictMode -Version Latest
+
+$private:releaseArgs = @{
+    Namespace = $env:RELEASE_NAMESPACE
+    Repository = $env:RELEASE_REPOSITORY
+    ApiKey = $env:GITHUB_API_TOKEN
+    TagName = $env:RELEASE_TAG_REF
+    TargetCommitish = git rev-parse $env:RELEASE_TAG_REF
+    Name = $env:RELEASE_TAG_REF
+    Body = Get-Content (if ($env:RELEASE_NOTES_PATH) { $env:RELEASE_NOTES_PATH } else { "$(git rev-parse --show-toplevel)\.release-notes.md" | Resolve-Path }) -Raw
+    Draft = $false
+    Prerelease = $false
+}
+
+try {
+    Push-Location $PSScriptRoot
+    Import-Module "$(git rev-parse --show-toplevel)\lib\PSGitHubRestApi\src\PSGitHubRestApi\PSGitHubRestApi.psm1" -Force -Verbose
+
+    # Create GitHub release
+    $response = New-GitHubRepositoryRelease @private:releaseArgs
+    $response
+
+}catch {
+    throw
+}finally {
+    Pop-Location
+}
