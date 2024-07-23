2bb13ec0940c38f3b21d354523549e4a5e8a6713
Joel Timothy Oh <joeltimothyoh@live.com>
2024-07-23 13:01:35 +0000

    Feature (generate): Improve commit subcategory support

--- src/PSRepositoryReleaseManager/generate/variants/VersionDate-HashSubject-NoMerges-CategorizedSorted.ps1
+++ src/PSRepositoryReleaseManager/generate/variants/VersionDate-HashSubject-NoMerges-CategorizedSorted.ps1
@@ -75,7 +75,7 @@ function VersionDate-HashSubject-NoMerges-CategorizedSorted {
         $commitHistoryCategorizedCollection = New-Object System.Collections.ArrayList
         $commitHistoryUncategorizedCollection = New-Object System.Collections.ArrayList
         $commitHistoryCollection | % {
-            if ($_ -match "^[0-9a-f]+ (\s*\w+\s*)(\(\s*[a-zA-Z0-9_-]+\s*\)\s*)*:(.+)") {
+            if ($_ -match "^[0-9a-f]+ (\s*\w+\s*)(\(\s*[a-zA-Z0-9_\-\/]+\s*\)\s*)*:(.+)") {
                 $commitHistoryCategorizedCollection.Add($_) > $null
             }else {
                 $commitHistoryUncategorizedCollection.Add($_) > $null
@@ -106,7 +106,7 @@ function VersionDate-HashSubject-NoMerges-CategorizedSorted {
             foreach ($c in $commitHistoryCategory) {
                 $iscommitHistoryCategoryTitleOutputted = $false
                 $commitHistoryCategorizedCustomCollection | Sort-Object -Property Subject | % {
-                    if ("$($_.Ref) $($_.Subject)" -match "^[0-9a-f]+ (\s*$($c['Name'])\s*)(\(\s*[a-zA-Z0-9_-]+\s*\)\s*)*:(.+)") {
+                    if ("$($_.Ref) $($_.Subject)" -match "^[0-9a-f]+ (\s*$($c['Name'])\s*)(\(\s*[a-zA-Z0-9_\-\/]+\s*\)\s*)*:(.+)") {
                         if (!$iscommitHistoryCategoryTitleOutputted) {
 @"
 
